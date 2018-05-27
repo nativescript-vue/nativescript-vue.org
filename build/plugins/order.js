eec3eef768eb550a992203fe647218fe403e7d0f
Iva Koevska <admatha@gmail.com>
2018-05-27 21:12:29 +0300

    Console log

--- build/plugins/order.js
+++ build/plugins/order.js
@@ -10,13 +10,12 @@ function plugin() {
 
 
       const res = path.basename(file).match(/^(\d+)-(\d+)-(\d+)/);
-      console.log(res);
       if (res) {
         const data = files[file];
         data.order = res[1];
-
+        console.log(res[1]);
         data.slug = data.slug.replace(res[0], '');
-
+        console.log(res[0]);
         // rename file to not include the order
         metalsmith.rename(file, file.replace(res[0], ''));
       }
