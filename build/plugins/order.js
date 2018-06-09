9f5386a022d00df837b7f6c9767fa2f83fad1bce
Igor Randjelovic <rigor789@gmail.com>
2018-06-09 14:59:21 +0200

    refactor: use non-capturing groups
    Use non-capturing groups because we don't need to manipulate the groups separately

--- build/plugins/order.js
+++ build/plugins/order.js
@@ -9,7 +9,7 @@ function plugin() {
       }
 
 
-      const res = path.basename(file).match(/^((\d+)-)+/);
+      const res = path.basename(file).match(/^(?:(?:\d+)-)+/);
       if (res) {
         const data = files[file];
         data.order = res[0].replace(/-/g, '');
