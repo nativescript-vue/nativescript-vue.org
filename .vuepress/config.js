3105a053e8dcaca5b3e0188e11e2bdcc97616c9b
jlooper <jen.looper@gmail.com>
2018-11-17 22:40:09 +0200

    Adding Tailwind CSS

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,38 +1,36 @@
 module.exports = {
-  themeConfig: {
-    nav: [
-      {text: 'Home', link: '/'},
-      {text: 'Docs', link: '/docs/en/introduction'},
-    ],
-    sidebar: {
-      '/docs/': [
-        '/docs/en/introduction',
-        {
-          title: 'Elements/ActionBar',
-          collapsable: false,
-          children: [
-            '/docs/en/elements/action-bar/action-bar',
-            '/docs/en/elements/action-bar/action-item',
-            '/docs/en/elements/action-bar/navigation-button',
-          ]
-        },
-        {
-          title: 'Elements/Components',
-          collapsable: false,
-          children: [
-            '/docs/en/elements/components/activity-indicator'
-          ]
-        }
-      ]
-    }
-  },
-  locales: {
-    // The key is the path for the locale to be nested under.
-    // As a special case, the default locale can use '/' as its path.
-    '/': {
-      lang: 'en-US', // this will be set as the lang attribute on <html>
-      title: 'VuePress',
-      description: 'Vue-powered Static Site Generator'
-    },
-  }
-}
+	postcss: {
+		plugins: [require('tailwindcss')('./tailwind.config.js'), require('autoprefixer')],
+	},
+	themeConfig: {
+		nav: [{ text: 'Home', link: '/' }, { text: 'Docs', link: '/docs/en/introduction' }],
+		sidebar: {
+			'/docs/': [
+				'/docs/en/introduction',
+				{
+					title: 'Elements/ActionBar',
+					collapsable: false,
+					children: [
+						'/docs/en/elements/action-bar/action-bar',
+						'/docs/en/elements/action-bar/action-item',
+						'/docs/en/elements/action-bar/navigation-button',
+					],
+				},
+				{
+					title: 'Elements/Components',
+					collapsable: false,
+					children: ['/docs/en/elements/components/activity-indicator'],
+				},
+			],
+		},
+	},
+	locales: {
+		// The key is the path for the locale to be nested under.
+		// As a special case, the default locale can use '/' as its path.
+		'/': {
+			lang: 'en-US', // this will be set as the lang attribute on <html>
+			title: 'VuePress',
+			description: 'Vue-powered Static Site Generator',
+		},
+	},
+};
---
+++ .vuepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+<a href="https://www.youtube.com/watch?v=QtGBCj6gCLY?source=ns-vue"
+   target="_blank"
+   rel="noopener"
+   class="block bg-blue-light md:bg-green md:border-t-8 border-green p-4 md:p-0 text-center no-underline text-white">
+   NativeScript-Vue 2.0: Back to the Vueture (Webinar) is now on YouTube
+</a>
+</template>
+
+<style lang="stylus">
+@import './styles/theme.styl';
+</style>
